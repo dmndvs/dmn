5f4ebe2037c41e5274a2f1a99e52842463a52e86
dmndvs <domin.dvs@gmail.com>
2015-12-16 20:16:17 +0200

    update2

--- dmn.js
+++ dmn.js
@@ -1,24 +1,42 @@
 (function($) {
+    /**
+     * Add placeholder to input textfield
+     * @type {{attach: Drupal.behaviors.dmn.attach}}
+     */
     Drupal.behaviors.dmn = {
         attach: function () {
-            /*
-                jQuery('#edit-dmn-fname').attr('placeholder','First Name');
-                jQuery('#edit-dmn-lname').attr('placeholder','Last Name');;*/
             jQuery("#dmn-fieldset input[name=dmn_fname]").attr('placeholder','First Name');
-            jQuery("#dmn-fieldset input[type=text]:eq(1)").attr('placeholder','Last Name');
+            jQuery("#dmn-fieldset input[name=dmn_lname]").attr('placeholder','Last Name');
         }
     };
-
+    /**
+     * @param ajax
+     * @param response
+     * @param status
+     */
     Drupal.ajax.prototype.commands.dmn_rebuild = function(ajax, response, status){
 
             jQuery('#' + response.elementId).replaceWith('<div>hello, ' + response.fname + ' ' + response.lname + '</div>');
 
     };
-
+    /**
+     * Redirect page to 'response'
+     *
+     * @param ajax
+     * @param response
+     * @param status
+     */
     Drupal.ajax.prototype.commands.dmn_redirect = function(ajax, response, status){
         window.location = response.topath;
     };
-
+    /**
+     * Example from web.
+     * see url = /dmn/view2
+     *
+     * @param ajax
+     * @param response
+     * @param status
+     */
     Drupal.ajax.prototype.commands.special_effects = function(ajax, response, status){
         jQuery('#' + response.elementId).hide().text(response.name).fadeIn(response.duration, function() {
             var fontSize = parseInt($(this).css('font-size'));
